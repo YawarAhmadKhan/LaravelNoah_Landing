736ea9e1d003fe78c2dd91ca239099e8cb100639
Yawar Ahmad Khan <36065624+yawaRcan@users.noreply.github.com>
2024-04-04 17:11:47 +0500

    breeze and livewire scafholding

---
+++ app/Livewire/NavLinks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class NavLinks extends Component
+{
+    public function render()
+    {
+        return view('livewire.nav-links');
+    }
+}
